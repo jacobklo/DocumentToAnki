0cb08c61a27a1fc5128e84807b4c649e73f4dbe4
Jacob Lo <it@jacoblo.net>
2022-12-31 19:17:26 +0800

    refactor

--- myanki.py
+++ myanki.py
@@ -1,4 +1,4 @@
-
+from __future__ import annotations
 import genanki
 import hashlib, os
 from pathlib import Path
@@ -19,9 +19,9 @@ def docx_to_anki_notes(filename: str):
   root = convert_paragraphs_to_tree(pp)
 
   my_model = MyModel(filename+' Model', fields=[{'name': 'Question'}, {'name': 'Answer'}, {
-      'name': 'Media'}, {'name': 'Path'}], front_html=QUESTION, back_html=ANSWER, css=STYLE)
+      'name': 'Media'}, {'name': 'TableOfContent'}], front_html=QUESTION, back_html=ANSWER, css=STYLE)
 
-  notes = create_node_to_anki_notes(root, my_model, filename)
+  notes = createAnkiNotes(root, my_model, filename)
 
   my_deck = genanki.Deck(deck_id=abs(hash(filename)) % (10 ** 10), name=filename)
 
@@ -53,64 +53,102 @@ class MyModel(genanki.Model):
 
 
 
-def create_node_to_anki_notes(root: Node, model: genanki.Model, filename: str) -> List[genanki.Note]:
+def createAnkiNotes(root: Node, model: genanki.Model, filename: str) -> List[genanki.Note]:
   if not root: return []
   filename = filename.lower().replace(' ','_')
   allNotes = _create_node_to_notes(root, 0, [])
   results = []
   for n in allNotes:
-    newNotes = genanki.Note(model=model, fields=[n[0], n[1], n[2], n[3]], tags=n[4]+[filename])
+    newNotes = genanki.Note(model=model, fields=[n.question, n.answer, n.media, n.tableOfContent], tags=n.tags+[filename])
     results += [newNotes]
   return results
 
 
-def _get_question_answer_branch(node: Node):
-  question, answer = '', ''
-  branch = NodeToAnki.unicodeToHTMLEntities(node.getBranchStr())
-  # So Document can save a line into multiple context, this is to add them all. For example: "This is <bold>one</bold> line" has 3 contexts
-  for p in node.context:
-    question += NodeToAnki.convertParagraphToHtml(p, True) + '<br>'
-    answer += NodeToAnki.convertParagraphToHtml(p, False) + '<br>'
-  return (question, answer, branch)
-
-
-def _create_node_to_notes(root: Node, current_level: int, all_photos_from_parent: List[PhotoNode]) -> List[Tuple[str, str, str, str, List[str]]]:
+def _create_node_to_notes(n: Node, curLevel: int, allPhotosFromParent: List[PhotoNode]) -> List[MyNote]:
   result = []
   # Check if it is one-off photo. It is one line of note that has a photo. it is identify with '®®0'
-  if isinstance(root, PhotoNode) and root.showOnChildrenLevel == 0:
-    question, answer, branch = _get_question_answer_branch(root)
-    media = '<img src="' + root.imageName + '"><br>'
-    tags = root.get_tags()
-    result += [(question, answer, media, branch, tags)]
+  if isinstance(n, PhotoNode) and n.showOnChildrenLevel == 0:
+    question, answer, tableOfContent = NodeToAnki.getAnkiNoteFields(n)
+    media = '<img src="' + n.imageName + '"><br>'
+    tags = n.get_tags()
+    result += [MyNote(question, answer, media, tableOfContent, tags)]
     return result
 
   # Check if there is a multi-line single Node, which is identify using '©©' and
   # Check if node is text paragraph
-  if len(root.context) > 0 and DocxToNode.isNormalParagraph(root.context[0]) and '®®' not in root.context[0].text and isinstance(root.context, List):
-    question, answer, branch = _get_question_answer_branch(root)
-    tags = root.get_tags()
+  if len(n.context) > 0 and DocxToNode.isNormalParagraph(n.context[0]) and '®®' not in n.context[0].text and isinstance(n.context, List):
+    question, answer, tableOfContent = NodeToAnki.getAnkiNoteFields(n)
+    tags = n.get_tags()
     media = ''
     # For all photos that Parent and grandparent and up contains, add into Anki note as well, because it may have info that need for that line/note
-    for pic in all_photos_from_parent:
+    for pic in allPhotosFromParent:
       # Right after ®®, there is an integer, ®®1 means show pic on lines/notes that is same level of this tree, ®®2 shows on this level and children nodes.
-      if pic.level - current_level < pic.showOnChildrenLevel:
+      if pic.level - curLevel < pic.showOnChildrenLevel:
         media += '<img src="' + pic.imageName + '"><br>'
 
     # create note
-    result += [(question, answer, media, branch, tags)]
+    result += [MyNote(question, answer, media, tableOfContent, tags)]
 
   new_all_photos_children = []
   # Append all photos of next children level here, so children level recursive call on each text node/note/line has all records of pic on that level.
-  for c in root.children:
+  for c in n.children:
     if isinstance(c, PhotoNode) and c.showOnChildrenLevel > 0:
       new_all_photos_children.append(c)
 
   # recursive call each children, also appends this level pics, just in case they need to show on gran-children level
-  for c in root.children:
-    result += _create_node_to_notes( c, current_level + 1, all_photos_from_parent + new_all_photos_children)
+  for c in n.children:
+    result += _create_node_to_notes( c, curLevel + 1, allPhotosFromParent + new_all_photos_children)
 
   return result
 
+
+
+class NodeToAnki:
+  @staticmethod
+  def unicodeToHTMLEntities(text: str) -> str:
+    """
+    Replace space and nextline character to HTML entity
+    """
+    return text.replace(os.linesep, '<br>').replace('\t', '&ensp;')
+  
+  @staticmethod
+  def convertParagraphToHtml(para: Paragraph, hideBold: bool) -> str:
+    """
+    Convert Node text to HTML format, where Anki can read.
+
+    :para:: python-docx Paragraph. Like a sentence
+    :hideBold:: If true, all the bold/italic text will all turn into _
+    """
+    result = ''
+    if not DocxToNode.isNormalParagraph(para) or DocxToNode.isEmptyParagraph(para):
+      return ''
+    for r in para.runs:
+      if r.bold:
+        result += '<b>' + ('_' * len(r.text) if hideBold else r.text) + '</b>'
+      elif r.italic:
+        result += '<i>' + ('_' * len(r.text) if hideBold else r.text) + '</i>'
+      else:
+        result += r.text
+    return result
+
+  @staticmethod
+  def getAnkiNoteFields(node: Node):
+    question, answer = '', ''
+    tableOfContent = NodeToAnki.unicodeToHTMLEntities(node.getBranchStr())
+    # So Document can save a line into multiple context, this is to add them all. For example: "This is <bold>one</bold> line" has 3 contexts
+    for p in node.context:
+      question += NodeToAnki.convertParagraphToHtml(p, True) + '<br>'
+      answer += NodeToAnki.convertParagraphToHtml(p, False) + '<br>'
+    return (question, answer, tableOfContent)
+
+
+class MyNote:
+  def __init__(self, question, answer, media, tableOfContent, tags):
+    self.question = question
+    self.answer = answer
+    self.media = media
+    self.tableOfContent = tableOfContent
+    self.tags = tags
 
 
 
@@ -123,7 +161,7 @@ ANSWER = '''
 <div class="back">
 <table style="width:100%">
   <tr>
-    <th>{{Path}}</th>
+    <th>{{TableOfContent}}</th>
   </tr>
   <tr>
     <th>{{Answer}}</th>
@@ -165,36 +203,6 @@ th {
 
 @font-face { font-family: DejaVu Sans Mono; src: url('_DejaVuSansMono.ttf'); }
 '''
-
-
-class NodeToAnki:
-  @staticmethod
-  def unicodeToHTMLEntities(text: str) -> str:
-    """
-    Replace space and nextline character to HTML entity
-    """
-    return text.replace(os.linesep, '<br>').replace('\t', '&ensp;')
-  
-  @staticmethod
-  def convertParagraphToHtml(para: Paragraph, hideBold: bool) -> str:
-    """
-    Convert Node text to HTML format, where Anki can read.
-
-    :para:: python-docx Paragraph. Like a sentence
-    :hideBold:: If true, all the bold/italic text will all turn into _
-    """
-    result = ''
-    if not DocxToNode.isNormalParagraph(para) or DocxToNode.isEmptyParagraph(para):
-      return ''
-    for r in para.runs:
-      if r.bold:
-        result += '<b>' + ('_' * len(r.text) if hideBold else r.text) + '</b>'
-      elif r.italic:
-        result += '<i>' + ('_' * len(r.text) if hideBold else r.text) + '</i>'
-      else:
-        result += r.text
-    return result
-
 
 
 if __name__ == "__main__":
