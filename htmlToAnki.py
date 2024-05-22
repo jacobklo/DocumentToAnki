9850c94484845d1b4298efd9c72c131cf8634e03
Jacob Lo <8814304+jacobklo@users.noreply.github.com>
2024-05-22 04:34:01 -0700

    gen anki cards for python docs

--- htmlToAnki.py
+++ htmlToAnki.py
@@ -1,29 +1,26 @@
 import xml.etree.ElementTree as ET
+from typing import List, Callable
+
+from myanki import MyModel
+
+import genanki
 
 
-def node_str(node, depth):
-    return f'  {" " * depth}  {node.tag} {node.attrib}\n'
+def node_str(node):
+    return f'{node.tag} {node.attrib} Children: {len(node)} {[n.tag for n in node]}\n'
 
 
-def node_str2(node, depth):
-    if node.tag == 'section':
-        return f'{node.tag} {node.attrib} Children: {len(node)} {[n.tag for n in node]}\n'
-    return ''
-
-
-def check_contain_attr(node_attr_str, list_of_attr) -> bool:
-    for attr in list_of_attr:
-        if attr in node_attr_str:
+def check_contain_attr(node_attr: str, attrs: List) -> bool:
+    for attr in attrs:
+        if attr in node_attr:
             return True
     return False
 
-def child_recursive(node: ET.Element, depth: int, callback) -> str:
-    result = ''        
-    
-    if node.tag == 'section' and 'id' in node.attrib and node.attrib['id'] == 'built-in-types':
-        print("built-in-types found")
 
+def child_recursive(node: ET.Element, parent_node_data: List, callback: Callable):
+     
     simple_text_children, complex_element_children = [], []
+
     for child in node:
         if child.tag in ['span', 'p', 'ul', 'ol', 'dt', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
             simple_text_children.append(child)
@@ -33,32 +30,61 @@ def child_recursive(node: ET.Element, depth: int, callback) -> str:
     for child in simple_text_children + complex_element_children:
         node.remove(child)
 
-    result += callback(node, depth)
-      
     for child in node:
-        result += child_recursive(child, depth + 1, callback)
+        this_node_data = ''
+        if child.attrib and 'id' in child.attrib:
+            this_node_data = child.attrib['id']
+        child_recursive(child, parent_node_data + [this_node_data], callback)
     
     red_box_div = ET.Element('div')
-    red_box_div.attrib['style'] = 'border-style: dashed; border-width: 5px; border-color: red;'
+    red_box_div.attrib['style'] = 'border-style: dotted; border-width: 5px; border-color: red;'
+    red_box_div.text = f'=== {parent_node_data} ===\n'
     red_box_div.extend(simple_text_children)
     node.insert(0, red_box_div)
 
     for child in complex_element_children:
         blue_box_div = ET.Element('div')
-        blue_box_div.attrib['style'] = 'border-style: dashed; border-width: 5px; border-color: blue;'
+        blue_box_div.attrib['style'] = 'border-style: dotted; border-width: 5px; border-color: blue;'
+        blue_box_div.text = f'=== {parent_node_data} ===\n'
         blue_box_div.append(child)
         node.append(blue_box_div)
     
-    return result
+    for n in node:
+        callback(n)
 
+
+
+def node_to_anki(nodes: List[ET.Element]):
+    filename = 'Python Docs'
+    my_model = MyModel(filename, fields=[{'name': 'Question'}, {'name': 'Answer'}, {
+      'name': 'Media'}, {'name': 'TableOfContent'}])
+
+    my_deck = genanki.Deck(deck_id=abs(hash(filename)) % (10 ** 10), name=filename)
+
+    for i, n in enumerate(nodes):
+        ET.ElementTree(n).write(f'out/{i}.html', encoding='utf-8')
+        
+        with open(f'out/{i}.html', 'r', encoding='utf-8') as f:
+            content = f.read()
+
+            anki_note = genanki.Note(model=my_model, fields=[content, content, '', ''], tags='')
+            my_deck.add_note(anki_note)
+
+    anki_output = genanki.Package(my_deck)
+    anki_output.write_to_file(filename+'.apkg')
+    
+    
 
 if __name__ == '__main__':
     tree = ET.parse('tmp.html')
 
-    out = child_recursive(tree.getroot(), 0, node_str2)
+    out_nodes = []
+    def callback(node):
+        out_nodes.append(node)
+    
+    child_recursive(tree.getroot(), [], callback)
 
-    with open('out.txt', 'w', encoding='utf-8') as f:
-        f.write(out)
+    node_to_anki(out_nodes)
     
     tree.write('out.html', encoding='utf-8')
 
